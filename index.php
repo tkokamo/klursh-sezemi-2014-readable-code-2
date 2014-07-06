b35f43e283a4ffc13a29e756a8ef494402aa0a06
klursh <ydt.hran2@gmail.com>
2014-07-06 11:50:50 +0900

    spec6: 表示するIDを指定できるようにした

--- index.php
+++ index.php
@@ -7,10 +7,19 @@ $recipe_names = lines_from_file(RECIPE_FILE_PATH);
 // それぞれIDを振って$recipesに入れる
 $recipes = array();
 for ($id = 0, $cnt = count($recipe_names); $id < $cnt; $id++) {
-    $recipes[] = new Recipe($id, $recipe_names[$id]);
+    $recipes[$id] = new Recipe($id, $recipe_names[$id]);
 }
 
-echo_array_as_lines($recipes);
+if (isset($argv[1])) {
+    // IDが指定された
+    // IDは数値
+    $id = intval($argv[1]);
+    echo $recipes[$id]."\n";
+}
+else {
+    // IDが指定されなかった。
+    echo_array_as_lines($recipes);
+}
 
 
 /**
