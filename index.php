51556f9db6a5ef73b611028471361782f0f8dc16
klursh <ydt.hran2@gmail.com>
2014-07-06 11:29:42 +0900

    spec4: 改行区切りのを複数のレシピとして読み込むようにした

--- index.php
+++ index.php
@@ -2,5 +2,15 @@
 
 define('RECIPE_FILE_PATH', 'data/recipe.txt');
 
-$str_recipe = file_get_contents(RECIPE_FILE_PATH);
-echo $str_recipe;
+$recipes = lines_from_file(RECIPE_FILE_PATH);
+// $recipesを全表示
+for ($i = 0, $count = count($recipes); $i < $count; $i++) {
+    echo $recipes[$i]."\n";
+}
+
+/**
+ * ファイルから1行ずつ読み込んで配列を返す。
+ */
+function lines_from_file($path) {
+    return file($path, FILE_IGNORE_NEW_LINES);
+}
